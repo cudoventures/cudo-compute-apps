35747fdc71fdf6ee3c235d39841fa2b81d8d04b4
sean <sean.berry@cudoventures.com>
2024-09-13 12:33:31 +0100

    more kong

--- kong/config/kong_config.sh
+++ kong/config/kong_config.sh
@@ -1,36 +1,43 @@
-## UI
 
-curl -X POST http://kong:8001/services \
-      --data name=admin-ui \
-      --data host=kong \
-      --data port=8002 && echo
+echo configure
 
-curl -X POST http://kong:8001/services/admin-ui/routes \
-      --data paths[]=/admin-ui && echo
-
-curl -X POST http://kong:8001/routes/admin-ui/plugins \
-  --data name=basic-auth && echo
+# User
 
 curl -X POST http://kong:8001/consumers \
-  --data username=kong-user && echo
+  --data username=kong-user 
+
+curl -X POST http://kong:8001/consumers/kong-user/key-auth \
+  --data key=env_password
 
 curl -X POST http://kong:8001/consumers/kong-user/basic-auth \
   --data username=kong \
   --data password=env_password
 
+## UI
+curl -X POST http://kong:8001/services \
+      --data name=admin-ui \
+      --data host=kong \
+      --data port=8002 
+
+curl -X POST http://kong:8001/services/admin-ui/routes \
+      --data paths[]=/admin-ui \
+      --data name=admin-ui-route 
+
+curl -X POST http://kong:8001/routes/admin-ui-route/plugins \
+  --data name=basic-auth 
+
 ## API
 curl -X POST http://kong:8001/services \
       --data name=admin-api \
       --data host=kong \
-      --data port=8001 && echo
+      --data port=8001 
 
 curl -X POST http://kong:8001/services/admin-api/routes \
-      --data paths[]=/admin-api && echo
+      --data paths[]=/admin-api \
+      --data name=admin-api-route 
 
-curl -X POST http://kong:8001/routes/admin-api/plugins \
+curl -X POST http://kong:8001/routes/admin-api-route/plugins \
   --data name=key-auth \
-  --data config.key_names=apikey && echo
+  --data config.key_names=apikey 
 
-curl -X POST http://kong:8001/consumers/kong-user/key-auth \
-  --data key=env_password
 
