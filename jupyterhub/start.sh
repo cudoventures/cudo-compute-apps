2303362d269d3a99d68ab5101af905efb6150a76
sean <sean.berry@cudoventures.com>
2025-02-20 14:59:04 +0000

    jupyterhub

--- jupyterhub/start.sh
+++ jupyterhub/start.sh
@@ -1,4 +1,4 @@
 apt-get -y install python3 python3-dev git curl
 curl -L https://tljh.jupyter.org/bootstrap.py \
  | sudo -E python3 - \
- --admin admin --user-requirements-txt-url
+ --admin admin --user-requirements-txt-url https://raw.githubusercontent.com/cudoventures/cudo-compute-apps/refs/heads/main/jupyterhub/requirements.txt
