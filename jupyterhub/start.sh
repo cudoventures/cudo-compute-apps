959a153796e2f6708fe185df2712a5db17075049
sean <sean.berry@cudoventures.com>
2025-03-12 20:06:51 +0000

    jupyterhub lock

--- jupyterhub/start.sh
+++ jupyterhub/start.sh
@@ -1,6 +1,18 @@
 export DEBIAN_FRONTEND="noninteractive"
+
+wait_for_lock() {
+    while fuser /var/lib/dpkg/lock-frontend >/dev/null 2>&1; do
+        echo "Waiting for other package management process to finish..."
+        sleep 5
+    done
+}
+
+wait_for_lock
 apt-get update
+
+wait_for_lock
 apt-get -y install python3 python3-dev git curl
+
 curl -L https://tljh.jupyter.org/bootstrap.py \
  | sudo -E python3 - \
  --admin admin --user-requirements-txt-url https://raw.githubusercontent.com/cudoventures/cudo-compute-apps/refs/heads/main/jupyterhub/requirements.txt
