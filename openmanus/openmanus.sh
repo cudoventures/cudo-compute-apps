6aba9f46ebee6c1849ec945adc17bcb503d7bdf9
sean <sean.berry@cudoventures.com>
2025-03-20 10:45:47 +0000

    open manus config location

--- openmanus/openmanus.sh
+++ openmanus/openmanus.sh
@@ -1,4 +1,5 @@
 export DEBIAN_FRONTEND="noninteractive"
+cd ~
 mkdir -p ~/miniconda3
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ~/miniconda3/miniconda.sh
 bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
@@ -8,10 +9,11 @@ conda init --all
 export DEBIAN_FRONTEND="noninteractive"
 #conda create -n open_manus python=3.12
 #conda activate open_manus
+cd ~
 git clone https://github.com/mannaandpoem/OpenManus.git
 cd OpenManus
 sed -i 's/"headless": False/"headless": True/g' app/tool/browser_use_tool.py
-wget https://raw.githubusercontent.com/cudoventures/cudo-compute-apps/refs/heads/open-manus/openmanus/config.toml -O config/config.toml
+wget https://raw.githubusercontent.com/cudoventures/cudo-compute-apps/refs/heads/main/openmanus/config.toml -O config/config.toml
 pip install -r requirements.txt
 playwright install-deps
 playwright install
