8dca8dec04844f113e3b91f9a087070653f1adec
sean <sean.berry@cudoventures.com>
2025-04-22 16:52:57 +0100

    dify

--- dify/cudo-update-dify.sh
+++ dify/cudo-update-dify.sh
@@ -1,3 +1,5 @@
+rm -rf docker
+rm docker.tar
 git clone --no-checkout https://github.com/langgenius/dify.git cudo-tmp
 cd cudo-tmp
 git sparse-checkout init
@@ -7,32 +9,9 @@ cd ..
 cp -r cudo-tmp/docker docker
 rm -rf cudo-tmp
 
-yq eval '.services.ollama = {
-  "image": "ollama/ollama",
-  "volumes": [".volumes/ollama:/root/.ollama"],
-  "ports": ["127.0.0.1:11434:11434"],
-  "container_name": "ollama",
-  "deploy": {
-    "resources": {
-      "reservations": {
-        "devices": [{
-          "driver": "nvidia",
-          "count": "all",
-          "capabilities": ["gpu"]
-        }]
-      }
-    }
-  }
-},
-  "curl-service": {
-    "image": "curlimages/curl",
-    "depends_on": ["ollama"],
-    "entrypoint": ["/bin/sh", "-c", "/scripts/poll_and_pull.sh"],
-    "volumes": [".:/scripts"]
-  }
-}' -i docker/docker-compose.yaml
+yq eval-all 'select(fileIndex == 0) * select(fileIndex == 1)' docker/docker-compose.yaml ollama.yaml -i
 
 cp docker/.env.example docker/.env
 echo "Ollama added to docker-compose.yaml"
 
-tar -cf docker.tar -C cudo-tmp docker
+tar -cf docker.tar docker
